13750f99f6a4cb94b2dda11c7daaf0332cc7899b
mali-shoaib <165029373+mali-shoaib@users.noreply.github.com>
2024-04-02 15:31:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+//console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number:",
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
